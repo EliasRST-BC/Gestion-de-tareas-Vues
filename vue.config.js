726a996c3291435d440f2a2ee0a315481db3b683
Elias.S <EliasTroche2004@gmail.com>
2024-11-11 16:06:12 -0300

    all update

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-  ? 'Gesti-n-de-tarea-Vues'
+  ? 'Gestion-de-tareas-Vues'
   : '/'
 })
