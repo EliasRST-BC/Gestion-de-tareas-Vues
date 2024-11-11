989c6a1374705fc5382bd3740b35616091b5a95e
Elias.S <EliasTroche2004@gmail.com>
2024-11-11 16:42:13 -0300

    updated main.js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-// Importa Bootstrap CSS y los íconos de Bootstrap aquí
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
 
-Vue.config.productionTip = false
+// Importa Bootstrap CSS y Bootstrap Icons
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
-  render: h => h(App)
-}).$mount('#app')
+  render: h => h(App),
+}).$mount('#app');
