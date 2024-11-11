aaaed38bdbe6a30f9a0c4e4afa9e2deac8181344
Elias.S <EliasTroche2004@gmail.com>
2024-11-11 17:33:56 -0300

    Updates

--- src/views/AboutView.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="about">
-    <h1>Esta aplicación esta diseñada usando Vue y la Api de JSON, está pagina te permitira organizar tus tareas, agregar nuevas o extraerlas directamente desde la API</h1>
-  </div>
-</template>
